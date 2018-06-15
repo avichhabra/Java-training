2cddbedd4e9d99b0d0f9687c6f922877e8af329a
avichhabra <avnishchhabra5@gmail.com>
2018-06-15 12:13:21 +0530

    changed

--- Class 4/Prime.java
+++ Class 4/Prime.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 class Prime{
    public static void main(String[] args){
 	  int i,j;
+	  System.out.println("Enter a number");
 	  Scanner sc;
 	 sc =new Scanner(System.in);
 	 int n = sc.nextInt();
 	  int count =0;
+	  System.out.println("Prime numbers are :");
      for(i=2;i<=n;i++) {
 		 for (j=1;j<i;j++) {
 			 if (i%j==0)
 				 count++;
 			
 		 }
-	if(count==1)
-			 System.out.println(i);	
+	if(count==1)	 
+		 System.out.println(i);	
              count=0;		 
 	 }	 
 	 
