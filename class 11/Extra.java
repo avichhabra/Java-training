889423a019f8dbe48915375ede85ee93988b602b
avichhabra <avnishchhabra5@gmail.com>
2018-06-22 16:15:03 +0530

    classs 11 modified

---
+++ class 11/Extra.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+class Extra {
+	static ArrayList al =new ArrayList();
+	public static void dir(File file) throws IOException {	
+	
+				File[] arr1 = file.listFiles();
+				for(File file1 : arr1) {
+				if(file1.isDirectory()){
+					dir(file1);
+				}
+				else{
+					System.out.println(file1.getAbsolutePath());
+					al.add(file1.getAbsolutePath());
+				}
+				}
+			}
+			
+	public static void main(String[] args) throws IOException {
+		File f1 = new File("C:/Users/Ossn/Desktop/abc.txt");
+		f1.createNewFile();
+		File f = new File("C:/Users/Ossn/Desktop/Training");
+		if(f.isDirectory()){
+			dir(f);
+		}
+		else{
+
+		}
+		FileWriter fw = new FileWriter(f1);
+		BufferedWriter bw = new BufferedWriter(fw);
+		Iterator i = al.iterator();
+		while(i.hasNext()) {
+			bw.write((String)i.next());
+			bw.newLine();
+		}
+		bw.flush();
+		
+	}
+}
--- class 11/MyArray.java
+++ class 11/MyArray.java
@@ -12,7 +12,7 @@ class MyArray {
 		System.arraycopy(arr,0,newArray,0,arr.length);
 		arr=newArray;
 		newArray = null;
-		arr[old_size] = 15;
+		arr[old_size] = 12;
 	}
 	for(int i=0;i<arr.length;i++) {
 		System.out.println(arr[i]);
